f7a0cab0923a7d87ef90678c2f7c51c94df50f04
xhy0728 <155809581+xhy0728@users.noreply.github.com>
2024-01-06 16:54:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-parentcontrol  package/luci-app-parentcontrol;
+sed -i 's/192.168.1.1/10.18.18.1/g' package/base-files/files/bin/config_generate
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='Adore'' package/lean/default-settings/files/zzz-default-settings
